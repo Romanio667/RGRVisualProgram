7967462822a62b7e3d9e5677db88d6eaceddf855
Roman Miroshnichenko <roma11092000@gmail.com>
2022-05-19 23:42:08 +0700

    5 этап

---
+++ MiroshnichenkoCurs/MiroshnichenkoCurs/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiroshnichenkoCurs.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ MiroshnichenkoCurs/MiroshnichenkoCurs/Views/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+using System;
+
+namespace MiroshnichenkoCurs.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
